b88b1ff8f55eb48f6e14981d5c40a396c9872a7c
Lukas Kraft <rebootrequired42@gmail.com>
2026-03-21 18:07:01 +0100

    Fix scripts for assignment4 part2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -63,8 +63,6 @@ done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
-echo $OUTPUTSTRING > /tmp/assignment4-result.txt
-
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Check for the correct number of arguments
 if [ $# -lt 2 ]; then
